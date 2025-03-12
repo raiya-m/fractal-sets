bf65fafb20258cde664e1b5c982855fb2d9875ee
Raiya Minhas <raiya.minhas@gmail.com>
2025-03-12 08:03:48 -0700

    function declarations and structured program with comments

--- burning-ship/main.cpp
+++ burning-ship/main.cpp
@@ -2,3 +2,31 @@
 #include <fstream>
 #include <cmath> 
 
+/* 
+
+same concept as mandelbrot, except absolute values are taken
+
+*/
+
+//mapping to real function
+int map_to_real() {
+
+}
+
+//mapping to imaginary function
+int map_to_imaginary() {
+
+}
+//determining the escape value function
+int find_escape() {
+    
+}
+
+int main () {
+    //some variable definitions
+    //read from input.txt
+    //create ppm header from input.txt
+    //nested pixel-by-pixel loop & call functions
+    //within loop output map to set rgb values 
+    //output 
+}
