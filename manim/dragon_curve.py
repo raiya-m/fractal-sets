aae2ddebe369ece9971baed4769ee00cbadcb688
Raiya Minhas <raiya.minhas@gmail.com>
2026-05-24 11:12:39 -0400

    loop for generating next iteration + visualizing

--- manim/dragon_curve.py
+++ manim/dragon_curve.py
@@ -14,3 +14,39 @@ class DragonCurve(Scene):
         title.to_edge(UP)
 
         self.play(Write(title))
+
+        # fact! manim always uses 3d vectors even when the scene is 2d. we'll start with a single horizontal line segment.
+
+        points = [
+            np.array([-2.0, 0.0, 0.0]),
+            np.array([2.0, 0.0, 0.0])
+        ]
+
+        curve = self.make_curve(points) # pass through our points
+
+        self.play(Create(curve))
+
+        self.wait(0.5)
+
+        # each generation doubles our complexity. 
+        
+        for i in range (0, 13): 
+            points = self.next_dragon(points)
+
+            new_curve = self.make_curve(points)
+
+            new_curve.set_color_by_gradient(
+                PINK,
+                PURPLE,
+                BLUE
+            )
+
+            # generation label at bottom 
+
+            label = Text(
+                f"Generation {i}",
+                font_size=24,
+                color=WHITE
+            )
+
+            label.to_edge(DOWN)
